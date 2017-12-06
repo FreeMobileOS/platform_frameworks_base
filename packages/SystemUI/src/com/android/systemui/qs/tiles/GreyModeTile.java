279579f631a623df4f024a172006e59003a105cc
Bernhard Rosenkränzer <bero@lindev.ch>
2017-12-06 20:40:53 +0100

    Adapt GreyMode to 8.1.0 API changes
    Signed-off-by: Bernhard Rosenkränzer <bero@lindev.ch>
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/GreyModeTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/GreyModeTile.java
@@ -75,7 +75,7 @@ public class GreyModeTile extends QSTileImpl<BooleanState> {
     }
 
     @Override
-    public void setListening(boolean listening) {
+    public void handleSetListening(boolean listening) {
         mSetting.setListening(listening);
         mDaltonizerMode.setListening(listening);
     }
