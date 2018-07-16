5462a48064cd537aeba325b093c7e348a06aeab4
Gopal Krishna Shukla <gshukla@codeaurora.org>
2018-07-16 18:05:32 +0530

    base: Fix System Server Crash in completePauseLocked()
    Check if WindowContainerController is NULL before
    calling stopFreezingScreenLocked() to avoid NPE.
    
    BUG:111489245
    Test: manual -Device booted up and running
    
    Change-Id: Ifb1b0430fcf54d51e5f67286fd38031bc8027069
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- services/core/java/com/android/server/wm/ActivityStack.java
+++ services/core/java/com/android/server/wm/ActivityStack.java
@@ -1827,7 +1827,7 @@ class ActivityStack extends ConfigurationContainer {
             // It is possible the activity was freezing the screen before it was paused.
             // In that case go ahead and remove the freeze this activity has on the screen
             // since it is no longer visible.
-            if (prev != null) {
+            if (prev != null && prev.mWindowContainerController != null) {
                 prev.stopFreezingScreenLocked(true /*force*/);
             }
             mPausingActivity = null;
