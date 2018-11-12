7fafd7eac107077a0a1ff620fb29cdcece1f3f2c
Satish Patel <tosatishpatel@gmail.com>
2018-11-12 18:16:07 +0530

    Add QS tiles for grey  mode
    Enable one click grey mode from quick tiles
    
    Signed-off-by: Satish Patel <tosatishpatel@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -30,6 +30,7 @@ import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
+import com.android.systemui.qs.tiles.GreyModeTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
@@ -76,6 +77,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new DndTile(mHost);
             case "inversion":
                 return new ColorInversionTile(mHost);
+            case "greymode":
+                return new GreyModeTile(mHost);
             case "airplane":
                 return new AirplaneModeTile(mHost);
             case "work":
